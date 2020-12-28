e97a293d86b48a17d4792c0880a67574eed38e19
bogdan <bogdan@reynencourt.com>
2020-12-29 00:10:56 +0100

    multiple return values

--- chapter5/findlinks1/main.go
+++ chapter5/findlinks1/main.go
@@ -1,5 +1,5 @@
 // findlinks1 prints the links in an html document read from standard input;
-package main
+package findlinks1
 
 import (
 	"fmt"
@@ -7,27 +7,30 @@ import (
 	"os"
 )
 
-// visit() traverses an HTML node tree, extracts the link from the href attribute of each anchor element,
+// Visit traverses an HTML node tree, extracts the link from the href attribute of each anchor element,
 // appends the links to a slice of strings, and returns the resulting slice:
-func visit(links []string, n *html.Node) []string {
+func Visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
+				if a.Val[0] != 104 {
+					continue
+				}
 				links = append(links, a.Val)
 			}
 		}
 	}
 
-	// to descend the tree for a node n, visit() recursively calls itself for each of n's children,
+	// to descend the tree for a node n, Visit() recursively calls itself for each of n's children,
 	// which are held in the FirstChild linked list;
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = visit(links, c)
+		links = Visit(links, c)
 	}
 
 	return links
 }
 
-func main() {
+func findlinks1() {
 
 	// html.Parse() reads a sequence of bytes, parses them, and returns the root of the HTML document tree,
 	// which is an html.Node
@@ -37,7 +40,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	for _, link := range visit(nil, document) {
+	for _, link := range Visit(nil, document) {
 		fmt.Println(link)
 	}
 
---
+++ chapter5/findlinks2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bodanc/gopl.io/chapter5/findlinks1"
+	"golang.org/x/net/html"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+/*	for _, url := range os.Args[1:] {
+		links, err := findLinks2(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "the function findlinks2 encountered an error: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}*/
+
+	for _, url := range os.Args[1:] {
+		links, err := findLinks2Log(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "the function findLinks2 encountered an error: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+
+}
+
+// findlinks2 performs an HTTP GET request for the url, parses the response as HTML,
+// and extracts and returns the links;
+func findLinks2(url string) ([]string, error) { // returns the list of discovered links
+	resp, err := http.Get(url) // resp is an http.Response type
+	if err != nil {
+		return nil, err // a nil []string
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// we must ensure that resp.Body is closed so that network resources are properly released,
+		// even in case of error;
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	return findlinks1.Visit(nil, doc), nil
+}
+
+// the multi-valued result returned by calling findLinks2() can be returned from a multi-valued calling function;
+func findLinks2Log(url string) ([]string, error) {
+	log.Printf("findLinks2 %s", url)
+	return findLinks2(url)
+}
