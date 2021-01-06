ad4cba80ae7b85faefcd9f3e60974a5eccc54542
bogdan <bogdan@reynencourt.com>
2021-01-07 00:19:42 +0100

    error handling

--- chapter5/tests/main.go
+++ chapter5/tests/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
-	"math"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 type Person struct {
@@ -14,26 +16,66 @@ type Person struct {
 
 func main() {
 
-	result1 := 42.6666
+	/*	result1 := 42.6666
 
-	result1 = add(10, 999)
-	fmt.Printf("%f\n", result1)
+		result1 = add(10, 999)
+		fmt.Printf("%f\n", result1)
 
-	result2 := subtract(16, 4)
-	fmt.Printf("%d\n", result2)
+		result2 := subtract(16, 4)
+		fmt.Printf("%d\n", result2)
 
-	s := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(addSlice(s))
+		s := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+		fmt.Println(addSlice(s))
 
-	bogdan := Person{
-		Name: "franz",
-		Age:  99,
+		bogdan := Person{
+			Name: "franz",
+			Age:  99,
+		}
+
+		changeName(bogdan)
+		fmt.Println(bogdan)
+
+		math.Sin(1)*/
+
+	response1, err := errorHandlingStrategy1()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(response1)
+
+	_, err = errorHandlingStrategy2()
+	if err != nil {
+		log.Println(err)
 	}
 
-	changeName(bogdan)
-	fmt.Println(bogdan)
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(file)
 
-	math.Sin(1)
+/*	err = wait.WaitForServer("https://nosuchthing.io")
+	fmt.Println(err)*/
+
+/*	// 3rd error handling strategy
+	if err := wait.WaitForServer("https://nosuchthing.io"); err != nil {
+		fmt.Fprintf(os.Stderr, "the url is down: %v\n", err)
+		os.Exit(1)
+	}*/
+
+/*	if err := wait.WaitForServer("https://nosuchthing.io"); err != nil {
+		// as with all the log functions, log.Fatalf prefixes the time and date to the error message
+		log.Fatalf("the url is down: %v\n", err)
+	}*/
+
+	dir, err := ioutil.TempDir("", "temp")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create temp dir: %v", err)
+	}
+	fmt.Println(dir)
+	// the call to os.RemoveAll may fail, but we can safely ignore it because the o/s periodically cleans out
+	// the temp dir;
+	os.RemoveAll(dir)
 
 }
 
